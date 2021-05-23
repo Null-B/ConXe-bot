f5f8ccc30aec598549f73d508cd3288dc04ab47e
Kemal <kemal.imeroski.08@gmail.com>
2021-05-23 23:44:24 +0200

    users join and leave

--- botcode.py
+++ botcode.py
@@ -1,38 +1,50 @@
+#Moduals
 import discord
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
+#deps
 TOKEN = os.getenv("TOKEN")
 PREFIX = '!'
 INTENTS = discord.Intents.default()
+
+#client
 client = discord.Client(commands_prefix=PREFIX, intents=INTENTS)
 
-
+#reddy message
 @client.event
 async def on_ready():
-    await client . change_presence(status=discord. Status . idle, activity=discord . Game("check my code here-https://github.com/Null-B/bot " ))
+    await client . change_presence(status=discord. Status . idle, activity=discord .Game("check my code here-https://github.com/Null-B/bot " ))
     print(f'Logged in as: {client.user.name}')
     print(f'With ID: {client.user.id}')
+
+#users join/leave /log
+client.event
+async def on_memberjoin(member,message): 
+    print(f'member has joined a server. ') 
+    await message.channel.send(member,'member has joined a server.')
+
+async def on_member_remove(member,message): 
+    print(f'member has left a server.' )
+    await message.channel.send(member,'member has left a server.')
+
+
 #comands
-#
+
 @client.event
 async def on_message(message):
     #help
-
+ 
     @client.event
     async def on_message(message):
 
+        #embed help list
         if message.content.startswith('$help'):
-            embedVar = discord.Embed(title="Comads", description="dont forget to use the \"$\" to use the comands", color=0x00ff00)
-            embedVar.add_field(name="Hi comads", value="$hi / $hello / $sup", inline=False)
-            embedVar.add_field(name="$penis", value="to be sus", inline=False)
-            embedVar.add_field(name="$haram", value="to see what is haram", inline=False)
-            # embedVar.add_field(name="Field2", value="hi2", inline=False)
-            # embedVar.add_field(name="Field2", value="hi2", inline=False)
-            # embedVar.add_field(name="Field2", value="hi2", inline=False)
-            # embedVar.add_field(name="Field2", value="hi2", inline=False)
-            await message.channel.send(embed=embedVar)
+                embedVar = discord.Embed(title="Comads", description="dont forget to use the \"$\" to use the comands", color=0x00ff00)
+                embedVar.add_field(name="Hi comads", value="$hi / $hello / $sup", inline=False)
+                await message.channel.send(embed=embedVar)
+        
 
         #hi comands
         if message.author == client.user:
@@ -48,14 +60,5 @@ async def on_message(message):
             await message.channel.send('sup my dued😀')
         
         #new
-
-        elif message.content.startswith('$penis'):
-            await message.channel.send("🍆")
-
-        elif message.content.startswith('$haram'):
-            await message.channel.send("🐖")
-
-
-
 
 client.run(TOKEN)
